55503cfb2ae1a46ac8dd3c18935b9cb5d4405366
Albert Diones <adiones@addteam.org>
2024-11-29 19:05:21 +0800

    mocked candleFetcher

--- tests/first.test.ts
+++ tests/first.test.ts
@@ -80,3 +80,69 @@ test(
         expect(candle.open).not.toBeFalsy();
     }
 );
+
+class MockCandleFetcher {
+    async fetchCandles(symbol: string, interval: number, limit: number): Promise<TickerCandle[] | null> {
+        return Promise.resolve([
+            {
+                open: 96820.5,
+                high: 96824.9,
+                low: 96820.5,
+                close: 96824.8,
+                base_volume: 0.56790463,
+                quote_volume: 54985.496884781,
+                open_timestamp: 1732877820000,
+                close_timestamp: 1732877879999,
+            },
+            {
+                open: 96820.5,
+                high: 96824.9,
+                low: 96820.5,
+                close: 96824.8,
+                base_volume: 0.56790463,
+                quote_volume: 54985.496884781,
+                open_timestamp: 1732877820000,
+                close_timestamp: 1732877879999,
+            }
+        ]);
+    }
+}
+
+
+test(
+    'cryptoBeep.getLatestClosedCandle()',
+    async () => {
+
+        const kuCoin: CandleFetcher = new KuCoin(
+            new HttpClient(
+                { 
+                    cache: new CacheViaNothing()
+                }
+            ),
+            {
+                logger: console
+            }
+        );
+
+        const beep = new CryptoBeep(
+            kuCoin,
+            'BTC-USDT', // symbol
+            1, // 1 minute candle
+            {
+                up: 'assets/up.wav',
+                down: 'assets/down.wav',
+            }
+        );
+
+        const candle = await beep.getLatestClosedCandle();
+        
+        expect(candle).not.toBeNull();
+
+        if (!candle) {
+            throw "Candle is null"
+        }
+        console.log(candle);
+        expect(candle.close).not.toBeFalsy();
+        expect(candle.open).not.toBeFalsy();
+    }
+);
